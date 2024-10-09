7c9317088e16f4db93926e1284ffbc92b94bc7e7
WonyeolHEO <hyy96hyy@naver.com>
2024-10-09 20:49:23 +0900

    Create project

---
+++ MapKit_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKit_SwiftUI
+//
+//  Created by wonyoul heo on 10/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKit_SwiftUI/MapKit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_SwiftUIApp.swift
+//  MapKit_SwiftUI
+//
+//  Created by wonyoul heo on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
