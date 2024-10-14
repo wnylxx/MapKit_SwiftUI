c267209e206b5800a9540fe9343ab2880117fd56
WonyeolHEO <hyy96hyy@naver.com>
2024-10-14 22:52:13 +0900

    폴더 구조 변경, 새로운 프로젝트 생성

---
+++ MapKit_MapApp_SwiftUI/MapKit_MapApp_SwiftUI/MapKit_MapApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_MapApp_SwiftUIApp.swift
+//  MapKit_MapApp_SwiftUI
+//
+//  Created by wonyoul heo on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_MapApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
